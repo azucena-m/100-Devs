5f42c75eccf21dcefbf71c41c449278fff7c2635
Azucena Marroquin <atrevizo4@cnm.edu>
2024-03-07 14:31:51 -0700

    made a cocktail api carousel

--- class26-materials/objects-constructor-fun/js/main.js
+++ class26-materials/objects-constructor-fun/js/main.js
@@ -1,1 +1,18 @@
-//Create a constructor with 4 properties and 3 methods
+//Create a constructor with 4 properties and 3 metho
+function Pizza(pizzaToppings, pizzaSize, pizzaSause, pizzaCheese){
+    this.toppings = pizzaToppings
+    this.size = pizzaSize
+    this.sause = pizzaSause
+    this.cheese = pizzaCheese
+    this.freezbe = function(){
+        console.log('yeet')
+    }
+    this.burn = function(){
+        console.log('aaauuuchhh')
+    }
+    this.delivery = function(){
+        console.log('on its way..')
+    }
+}
+
+let pepPizza = new Pizza('pepperoni', 'large', 'marinara', 'extra cheese')
